798f817179d4b2a0a27d109c6dabed976761d44e
Adam <lehreradam@yahoo.com>
2019-11-25 12:10:22 -0800

    changing static files

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client'));
+    app.use('/public', express.static('public'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
