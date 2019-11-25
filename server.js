b0ce8f1559cef3be0c501f508db264cad7c084a1
Adam <lehreradam@yahoo.com>
2019-11-25 12:15:16 -0800

    updating files

--- server.js
+++ server.js
@@ -35,13 +35,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use('/public', express.static('public'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-    });
+    app.use(express.static("client/build"));
   }
 
 
