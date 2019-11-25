107c5370c736ad82bff45df6b6b4c436549a1967
Adam <lehreradam@yahoo.com>
2019-11-25 11:49:17 -0800

    updating heroku build

--- server.js
+++ server.js
@@ -32,8 +32,17 @@ require('./config/passport')(passport); // pass passport for configuration
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("../client"));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
   app.use(session({
