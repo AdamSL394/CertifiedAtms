bde6562c6313bda4b84f98884704aa2e0589636d
Adam <lehreradam@yahoo.com>
2019-11-26 07:16:36 -0800

    fixing image spelling

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -4,7 +4,7 @@ import './style.css'
 function AboutUs (){
     return (
         <div id="homeContainer">
-           <img src={"./text.png"} alt="What we do and what you'll need" className="textimg"/>
+           <img src={"./Text.png"} alt="What we do and what you'll need" className="textimg"/>
            <img src={"./Graphs.png"} alt="Graphs showing revenue based at 70 cents" className="graphs"/>
         </div>
     )
